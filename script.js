9e3afdc94ebea75daf7377b07da738fa3ccc0d07
Dom McCabe <dommct@yahoo.co.uk>
2020-11-25 11:23:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
+
+// Set the slide number to 1- Then show the current slide
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous buttons
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image buttons
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
+  
+  /*Take all the slides, dots and captions in the document.
+  If slide number > number of slides then return to 1st slide.
+  If slide number is < total hide image.
+  Replace dots active status with inactive status.
+  Set current image to block display.
+  Set the caption text to match image.
+
+  */
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length)
-   {slideIndex = 1}
-  
-   if (n < 1)
-   {slideIndex = slides.length}
-  
-   for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
