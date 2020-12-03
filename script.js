81e0534aff7ccd53b0eb51fa5e1c80efef3f82d8
Dom McCabe <dommct@yahoo.co.uk>
2020-12-03 22:34:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-// Set the slide number to 1- Then show the current slide
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -14,26 +12,14 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  
-  /*Take all the slides, dots and captions in the document.
-  If slide number > number of slides then return to 1st slide.
-  If slide number is < total hide image.
-  Replace dots active status with inactive status.
-  Set current image to block display.
-  Set the caption text to match image.
-
-  */
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("slides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  
+  slides[slideIndex-1].style.display = "flex";
+  
 } 
